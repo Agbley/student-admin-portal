389568b2c1488db87b457e03e9e120c201da1f4b
moreirarobertson <moreira.robertson@gmail.com>
2025-10-12 15:07:43 +0100

    base 1 commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const studentRoutes = require("./routes/studentRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/students", studentRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/courses", require("./routes/courseRoutes"));
+app.use("/api/applications", require("./routes/applicationRoutes"));
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("Server started on port 5000")))
+  .catch((err) => console.log(err));
